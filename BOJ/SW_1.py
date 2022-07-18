3a2aac0bf2e84ea446fe4615e1716248ac6c86fa
hyebin <xxico89@gmail.com>
2022-07-18 20:24:23 +0900

    22.07.18

---
+++ BOJ/SW_1.py
@@ -0,0 +1,68 @@
+from pprint import pprint
+from collections import deque
+
+T = int(input())
+space = []
+d = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # 상하좌우
+spot = 0
+
+def check(x, y):
+    if space[x][y] != -1: count = 1
+    else: count = 0  # 근처 구역 count
+    spot = 0   # 구역 몇개인지
+    q = deque()
+    q.append((x, y))
+    check_visited[x][y] = True
+    while q:
+        x, y = q.popleft()
+        for i in range(4):
+            nx, ny = x + d[i][0], y + d[i][1]
+            if 0 <= nx < N and 0 <= ny < N and not check_visited[nx][ny]:
+                if space[nx][ny] != -1:
+                    q.append((nx, ny))
+                    count += 1
+                    check_visited[nx][ny] = True
+
+    return True
+
+
+def sink(x, y, year):           # 잠기는 땅 -1로 변경
+    global spot
+    queue = deque()
+    queue.append((x, y))
+    visited[x][y] = True
+    while queue:
+        x, y = queue.popleft()
+        for i in range(4):
+            nx, ny = x + d[i][0], y + d[i][1]
+            if 0 <= nx < N and 0 <= ny < N and not visited[nx][ny]:
+                print(nx, ny)
+                if space[nx][ny] <= year:
+                    space[nx][ny] = -1  # 물에 잠긴 표시
+                visited[nx][ny] = True
+                queue.append((nx, ny))
+
+    for i in range(len(space)):
+        for j in range(len(space[0])):
+            if space[i][j] != -1 and not check_visited[i][j]:
+                if check(i, j):
+                    spot += 1
+
+    return spot
+
+result = []
+max_spot = -1
+
+for _ in range(T):
+    N = int(input())
+    for i in range(N):
+        space.append(list(map(int, input().split())))
+    visited = [[False] * N for _ in range(N)]               # for sink
+    check_visited = [[False] * N for _ in range(N)]              # for check
+    for year in range(1, max(map(max, space)) + 1):
+        s = sink(0, 0, year)
+        max_spot = max(max_spot, s)
+    result.append(max_spot)
+
+for i, x in enumerate(result):
+    print('#'+i, x)
