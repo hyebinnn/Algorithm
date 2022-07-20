b932a719ff96ae49942b8d17f34cef3aa2784d46
hyebin <xxico89@gmail.com>
2022-07-20 18:02:25 +0900

    22.07.20

---
+++ BOJ/P_1052.py
@@ -0,0 +1,29 @@
+# 물병 (pypy3로 해야 맞음. 안그러면 시간초과)
+
+import sys
+
+n, k = map(int, sys.stdin.readline().split())
+
+
+def bottle(num):
+    b = 0                   # 총 물병 갯수
+    while 1:
+        twin = num // 2
+        remain = num % 2
+        b += remain
+        num = twin
+        if num == 0:              # 끝까지 1병으로 합쳤을 때
+            break
+    return b
+
+
+if k >= n:
+    print(0)
+else:
+    buy = n
+    while 1:
+        if bottle(buy) <= k:
+            print(buy - n)
+            break
+        else:
+            buy += 1
