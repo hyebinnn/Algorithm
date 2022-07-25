3245e03a9b3b28f313c38755cb67c79fa5718180
hyebin <xxico89@gmail.com>
2022-07-25 18:27:23 +0900

    22.07.25

---
+++ BOJ/P_21608.py
@@ -0,0 +1,49 @@
+# 상어 초등학교
+
+from collections import deque
+
+n = int(input())
+space = [[0]*n for _ in range(n)]            # 교실 공간
+like = dict()                               # 각 학생의 좋아하는 학생들 번호
+for _ in range(n**2):
+    temp = list(map(int, input().split()))
+    like[temp[0]] = temp[1:]
+
+d = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+# 현 좌석 주변에 있는 학생 수, 빈칸 수 return
+def check_near(x, y):
+    near = []
+    empty = 0
+    for i in range(4):
+        nx, ny = x + d[i][0], y + d[i][1]
+        if 0 <= nx < n and 0 <= ny < n:
+            if space[nx][ny] > 0:               # 주변에 있는 학생들 // 현 좌석 주변 모든 좌석이 현 학생이 좋아하는 학생인지 다 탐색한다면 시간초과 예상. -> 일단 주변에 있는 학생들만 모아두고 나중에 확인하자
+                near.append(space[nx][ny])      # 주변 학생의 번호
+            if space[nx][ny] == 0:
+                empty += 1
+
+    return near, empty
+
+
+
+
+for student in like:                # 자리를 선택할 학생 1명씩 (like의 키값들 전부 순회)
+    arr = []
+    for i in range(n):
+        for j in range(n):
+            like_cnt = 0
+            if space[i][j] != 0:
+                continue
+            near, empty = check_near(i, j)
+            for who in like[student]:
+                if who in near:
+                    like_cnt += 1
+            tmp = [like_cnt, empty, i, j]       # 현 좌석의 주변 좋아하는 학생 수, 빈칸 수, 현 좌석 좌표
+            arr.append(tmp)
+    arr = sorted(arr, key = lambda x: (-x[0], -x[1], x[2], x[3]))
+    print(student, arr)
+    x, y = arr[0][2], arr[0][3]
+    space[x][y] = student
+    print(space)
+
--- BOJ/test.py
+++ BOJ/test.py
@@ -1,4 +1,27 @@
+# num = [1, 2]
+# fruits = ['apple', 'banana']
+# color = ['red', 'yellow']
+# print(list(zip(num, fruits, color)))
+#
+# def solution(brown, yellow):
+#     answer = []
+#     arr = []                # 약수 들어가있는 리스트
+#     brown = brown - 4
+#     if yellow == 1:  answer = [3, 3]
+#     else:
+#         for x in range(1, yellow+1):
+#             if yellow % x == 0:
+#                 arr.append(x)
+#         for i in range(len(arr) // 2):
+#             h = arr[i]
+#             w = arr[-(i+1)]
+#             result = w*2 + h*2
+#             if result == brown:
+#                 answer = [w+2, h+2]
+#                 break
+#     return answer
+from collections import Counter
 
-
-a = 12.01
-print(int(a))
+s = {4: [1,3], 3: [1, 9, 4, 5], 9: [8, 1, 2, 3], 8: [1, 9, 3, 4], 7: [2, 3, 4, 8], 1: [9, 2, 5, 7], 6: [5, 2, 3, 4], 5: [1, 9, 2, 8], 2: [9, 3, 1, 4]}
+a = [(0,1), (0,0), (0,1), (2,4), (0,0), (3,6)]
+print(s[4][1])
